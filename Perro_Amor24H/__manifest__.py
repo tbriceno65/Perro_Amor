5de3a029d8461776bc40cdc08a68da3bc44f77d2
tbriceno65 <tbriceno@cimasproyectos.com>
2022-05-10 18:09:38 +0000

    [ADD] Basic Module

---
+++ Perro_Amor24H/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Perro Amor 24h'
+    
+    'summary': """Perro Amor 24h to manage veterinary services"""
+    
+    'description': """
+         Veterinary module to manage:
+         - Clinical history
+         - Profesional Commisions
+         - Patiens attendees
+    """
+    
+    'author': 'CIMAS Proyectos'
+    
+    'website':
+    
+    'category': 'Veterinary Services'
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+    
+}
